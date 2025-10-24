c346c589a29f6e32128748dbf6348eea2c511908
harlanfadhilah69 <harlancoegg@gmail.com>
2025-10-24 16:23:49 +0700

    mengubah ukuran logo menjadi size 130.dp dan menambah code untuk di run pada main activity

--- app/src/main/java/com/example/questadvancelayout_009/MainActivity.kt
+++ app/src/main/java/com/example/questadvancelayout_009/MainActivity.kt
@@ -20,8 +20,8 @@ class MainActivity : ComponentActivity() {
         setContent {
             QuestAdvanceLayout_009Theme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
+                    ActivitasPertama(
+
                         modifier = Modifier.padding(innerPadding)
                     )
                 }
--- app/src/main/java/com/example/questadvancelayout_009/UIPertama.kt
+++ app/src/main/java/com/example/questadvancelayout_009/UIPertama.kt
@@ -64,7 +64,7 @@ fun ActivitasPertama(modifier: Modifier){
                     painter = gambar,
                     contentDescription = null,
                     modifier = Modifier
-                        .size(size = 100.dp)
+                        .size(size = 130.dp)
                         .padding(all = 5.dp)
                 )
 
