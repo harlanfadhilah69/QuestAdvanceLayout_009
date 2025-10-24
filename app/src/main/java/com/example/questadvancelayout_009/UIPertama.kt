1123e097546acebaadba63ee442412a12a461c97
harlanfadhilah69 <harlancoegg@gmail.com>
2025-10-24 16:05:21 +0700

    Menerapkan alignment BottomCenter pada elemen footer

--- app/src/main/java/com/example/questadvancelayout_009/UIPertama.kt
+++ app/src/main/java/com/example/questadvancelayout_009/UIPertama.kt
@@ -93,6 +93,7 @@ fun ActivitasPertama(modifier: Modifier){
             Text(
                 text = stringResource(id = R.string.copy),
                 modifier = Modifier
+                    .align(Alignment.BottomCenter)
             )
 
         }
